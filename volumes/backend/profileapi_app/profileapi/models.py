aa953c39ba95c30b4f0fa261d9d4317a613fa2aa
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-09-26 15:54:20 +0200

    signi form saved to profileapi

--- volumes/backend/profileapi_app/profileapi/models.py
+++ volumes/backend/profileapi_app/profileapi/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Profile(models.Model):
-    user_id = models.IntegerField(max_length=30) # This is the user_id from the authentif app
+    user_id = models.IntegerField() # This is the user_id from the authentif app
     city = models.CharField(max_length=100, blank=True, default='Málaga')
     country = models.CharField(max_length=100, blank=True, default='Spain')
     played_games = models.IntegerField(default=0)
--- volumes/backend/profileapi_app/profileapi/urls.py
+++ volumes/backend/profileapi_app/profileapi/urls.py
@@ -1,11 +1,38 @@
 from django.http import HttpResponse
 from django.urls import path
+from django.http import JsonResponse
 import logging
+import json
+from .models import Profile
 logger = logging.getLogger(__name__)
 
+
 def api_signup(request):
-    logger.debug("hello from api_signup")
-    return HttpResponse('api_signup', status=201)
+    logger.debug("--> hello from api_signup")
+    if (request.method != 'POST'):
+        logger.debug("Method not allowed")
+        return HttpResponse('Method not allowed', status=405)
+    csrf_token = request.COOKIES.get('csrftoken')
+    headers = {
+        'X-CSRFToken': csrf_token,
+        'Cookie': f'csrftoken={csrf_token}',
+        'Content-Type': 'application/json',
+        'HTTP_HOST': 'profileapi',
+    }
+    logger.debug("--> POST method")
+    data = json.loads(request.body)
+    logger.debug(f"data : {data['user_id']}")
+    try:
+      profile = Profile(
+      user_id=data['user_id'],
+      )
+      profile.save()
+      logger.debug("--> profile created")
+      return JsonResponse({'message': 'Signup successful'}, status=201)
+    except Exception as e:
+      return JsonResponse({'error': str(e)}, status=400)
+    else:
+      return JsonResponse({'error': 'Method not allowed'}, status=405)
 
 urlpatterns = [
     path('api/signup/', api_signup, name='api_signup'),
