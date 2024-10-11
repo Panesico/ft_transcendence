770ca26a08a9707fafe0f8ac8fc68a5c9c713e8d
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-10-11 18:19:58 +0200

    Notification displayed for friend request

--- volumes/backend/gateway_app/static_dev/js/main_room.js
+++ volumes/backend/gateway_app/static_dev/js/main_room.js
@@ -61,15 +61,51 @@ function sendFriendRequest(username, user_id)
 function parseSocketMessage(data)
 {
   if (data.type === 'friend_request') {
-    handleFriendRequest(data);
+    addNotification(data);
   }
 }
 
-function handleFriendRequest(data)
+function addNotification(data)
 {
+  const notificationDropdown = document.getElementById('navbarDropdownNotifications');
+  const notificationDropdownClass = document.getElementById('notificationClassContent');
   incoming_username = data.username;
   incoming_user_id = data.user_id;
-  console.log('handleFriendRequest > incoming_username:', incoming_username);
+
+  // Remove the 'no notifications' message
+  const emptyMessage = document.getElementById('notificationContent');
+  if (emptyMessage)
+  {
+    if (emptyMessage.innerHTML.trim() === 'You have no notifications') {
+      console.log('addNotification > Removing empty message');
+      emptyMessage.remove();
+    }
+    else {
+      console.log('addNotification > No empty message to remove');
+      console.log('addNotification > emptyMessage.innerHTML:', emptyMessage.innerHTML);
+    }
+
+  }
+
+  // Create a new notification element
+  const newNotification = document.createElement('li');
+  newNotification.classList.add('dropdown-item');
+  newNotification.textContent = `You have a friend request from ${data.username}`;
+  newNotification.style.color = 'red';
+  console.log('addNotification > incoming_user_id:', incoming_user_id);
+
+  // Change default empty message to the new notification
+  if (notificationDropdownClass) {
+    notificationDropdownClass.appendChild(newNotification);
+    console.log('addNotification > notificationDropdown:', notificationDropdown);
+    const bellIcon = notificationDropdown.querySelector('img');
+    if (bellIcon) {
+      bellIcon.src = '/media/utils_icons/bell_up.png';
+    }
+  }
+  else {
+    console.log('addNotification > notificationDropdown is null');
+  }
   // const friendRequestModal = document.getElementById('friendRequestModal');
   // const friendRequestModalContent = document.getElementById('friendRequestModalContent');
   // const friendRequestModalAccept = document.getElementById('friendRequestModalAccept');
