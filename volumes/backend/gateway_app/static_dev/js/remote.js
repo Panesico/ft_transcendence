9e98a72e13a4431bfcbe490074a8d608c00d332c
Doruk Emre <doruk.emre@outlook.com>
2024-10-11 13:38:21 +0200

    Added checkbox for when players are ready in remote game, game is in sync with calcgame

--- volumes/backend/gateway_app/static_dev/js/remote.js
+++ volumes/backend/gateway_app/static_dev/js/remote.js
@@ -30,13 +30,123 @@ async function startRemoteGame(
 }
 
 async function newRemoteGame(game_type, p1_name) {
+  // Set up the canvas
+  const canvas = document.createElement('canvas');
+  canvas.width = 900;
+  canvas.height = 550;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#d3d3d3';  // Set the fill color
+  ctx.strokeStyle = '#d3d3d3';
+
+  const ballSize = 15
+  const paddleWidth = 15
+  const paddleHeight = 80
+  const borderWidth = 15
+
+  const keys =
+      {w: false, s: false, 8: false, 5: false, ' ': false, Escape: false};
+
+  function displayCanvasElement() {
+    const gameContainer = document.querySelector('#game-container');
+    gameContainer.innerHTML = '';
+    gameContainer.appendChild(canvas);
+  }
+
+  function showCountdown(gameState, count) {
+    document.querySelector('.scorePlayer1').textContent =
+        gameState.scorePlayer1;
+    document.querySelector('.scorePlayer2').textContent =
+        gameState.scorePlayer2;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = '60px PixeloidSans';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(count, canvas.width / 2, canvas.height / 2);
+  }
+
+  // Render the game state on the canvas
+  function renderGame(gameState) {
+    document.querySelector('.scorePlayer1').textContent =
+        gameState.scorePlayer1;
+    document.querySelector('.scorePlayer2').textContent =
+        gameState.scorePlayer2;
+
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw center line
+    let centerLineY = 0;
+    while (centerLineY < canvas.height) {
+      ctx.fillRect(
+          canvas.width / 2, centerLineY + 0.5 * borderWidth, 1, borderWidth);
+      centerLineY += 2 * borderWidth;
+    }
+
+    // Draw top and bottom borders
+    ctx.fillRect(0, 0, canvas.width, borderWidth);
+    ctx.fillRect(0, canvas.height - borderWidth, canvas.width, borderWidth);
+
+    // Draw the left paddle
+    ctx.fillRect(
+        2 * paddleWidth, gameState.leftPaddleY, paddleWidth, paddleHeight);
+    // Draw the right paddle
+    ctx.fillRect(
+        canvas.width - 3 * paddleWidth, gameState.rightPaddleY, paddleWidth,
+        paddleHeight);
+
+    // Draw ball
+    ctx.fillRect(gameState.ballX, gameState.ballY, ballSize, ballSize);
+  }
+
   return new Promise((resolve, reject) => {
     const gameCalcSocket = new WebSocket('/wss/calcgame/pong/remote/');
+    let game_id = 0;
+
+
+    function setPlayerReadyCheckBoxes(player_role) {
+      const player1Ready = document.getElementById('player1-ready');
+      const player2Ready = document.getElementById('player2-ready');
+      if (player_role === '1') {
+        player1Ready.disabled = false;
+        player1Ready.addEventListener(
+            'click', () => togglePlayerReady(player_role));
+      } else if (player_role === '2') {
+        player2Ready.disabled = false;
+        player2Ready.addEventListener(
+            'click', () => togglePlayerReady(player_role));
+      }
+    }
+
+    function updateOpponentReadyCheckBoxes(opponent) {
+      const player1Ready = document.getElementById('player1-ready');
+      const player2Ready = document.getElementById('player2-ready');
+      if (opponent === '1') {
+        player1Ready.checked = true;
+      } else if (opponent === '2') {
+        player2Ready.checked = true;
+      }
+    }
+
+    function togglePlayerReady(player_role) {
+      const player1Ready = document.getElementById('player1-ready');
+      const player2Ready = document.getElementById('player2-ready');
+      if (player_role === '1' && player1Ready.checked) {
+        gameCalcSocket.send(
+            JSON.stringify({type: 'player_ready', player: 'player1', game_id}));
+        player1Ready.disabled = true;
+      } else if (player_role === '2' && player2Ready.checked) {
+        gameCalcSocket.send(
+            JSON.stringify({type: 'player_ready', player: 'player2', game_id}));
+        player2Ready.disabled = true;
+      }
+    }
 
     gameCalcSocket.onopen = function(e) {
+      //
       console.log('newRemoteGame > .onopen, connection opened.');
-      gameCalcSocket.send(
-          JSON.stringify({type: 'opening_connection', p1_name: p1_name}));
+      gameCalcSocket.send(JSON.stringify(
+          {type: 'opening_connection, my name is', p1_name: p1_name}));
     };
 
     gameCalcSocket.onmessage = function(e) {
@@ -53,7 +163,24 @@ async function newRemoteGame(game_type, p1_name) {
             ' player_role:', data.player_role);
         // Load game html
         document.querySelector('main').innerHTML = data.html;
-        announceGame(data.message, data.player_role);
+        game_id = data.game_id;
+        announceGame(data.title, data.message);
+        setPlayerReadyCheckBoxes(data.player_role);
+
+      } else if (data.type === 'opponent_ready') {
+        console.log('newRemoteGame > .onmessage opponent_ready:', data.message);
+        updateOpponentReadyCheckBoxes(data.opponent)
+
+
+      } else if (data.type === 'game_countdown') {
+        console.log('newRemoteGame > .onmessage game_countdown:', data.message);
+        if (data.countdown === 3) displayCanvasElement();
+        showCountdown(data.game_state, data.countdown);
+
+      } else if (data.type === 'game_update') {
+        // console.log('newRemoteGame > .onmessage game_update:', data.message);
+        renderGame(data.game_state);
+
       } else if (data.type === 'game_end') {
         console.log('newRemoteGame > .onmessage game_end:', data);
         resolve(data.game_result);
