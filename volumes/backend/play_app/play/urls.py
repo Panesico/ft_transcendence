5624c6a11bfcc070fd276f2e91696f4b5bdc11ad
Doruk Emre <doruk.emre@outlook.com>
2024-10-23 21:20:34 +0200

    Fixed game_type, winner_name and id when saving to db, and added winrate to player in remote

--- volumes/backend/play_app/play/urls.py
+++ volumes/backend/play_app/play/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # Save single game
   path('api/saveGame/', views.api_saveGame, name='saveGame'),
-  # path('api/newTournament/', views.api_newTournament, name='newTournament'),
 
+  # Create and update tournaments
   path('api/createTournament/', views.api_createTournament, name='createTournament'),
   path('api/updateTournament/', views.api_updateTournament, name='updateTournament'),
 
   path('api/getGames/<str:user_id>/', views.api_getUserGames, name='getGames'),
   path('api/getMatchMaking/<str:user_id>/', views.api_getMatchMaking, name='getMatchMaking'),
+
+  # Get winrate of a user for a given game type
+  path('api/getWinrate/<str:user_id>/<str:game_type>/', views.api_getWinrate, name='getWinrate'),
 ]
