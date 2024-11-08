cc17f3ea0e90b89030a31ac694671430e98d818f
Doruk Emre <doruk.emre@outlook.com>
2024-11-08 15:04:57 +0100

    Reverted Jorge's breaking changes. DO NOT REMOVE 405

--- volumes/backend/gateway_app/gateway/urls.py
+++ volumes/backend/gateway_app/gateway/urls.py
@@ -5,11 +5,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
-from django.conf.urls import handler404
+# from django.conf.urls import handler404
 
-handler404 = 'gateway.viewsErrors.get_404'
+# handler404 = 'gateway.viewsErrors.get_404'
+# handler405 = 'gateway.viewsErrors.get_405'
+
 
 urlpatterns = [
+    # errors pages
+    path('404/', viewsErrors.get_404, name='404'),
+    path('405/', viewsErrors.get_405, name='405'),
 
     # home page
     path('', views.get_home, name='home'),
