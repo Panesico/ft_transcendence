368d8f0e2cf22ea892cda8e3f2bbd9e23771b590
Panesico <pranmc7676@protonmail.com>
2024-11-08 12:16:01 +0100

    auth models are now imported in all containers

--- volumes/backend/calcgame_app/calcgame/settings.py
+++ volumes/backend/calcgame_app/calcgame/settings.py
@@ -55,12 +55,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'calcgame',
+	'calcgame',
     'channels',
     'uvicorn',
+    'authentif',
 ]
 
-# AUTH_USER_MODEL = 'authentif.User'
+AUTH_USER_MODEL = 'authentif.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
