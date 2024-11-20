28b0f85dd083f5638ea81ad3834abb11c61bd47c
iisaacc <isaacporras2001@gmail.com>
2024-11-20 12:55:34 +0100

    blockchain script

--- srcs/blockchain/tools/init_hardhat.sh
+++ srcs/blockchain/tools/init_hardhat.sh
@@ -1,20 +1,35 @@
 #! /bin/sh -l
-
 # Install hardhat dependencies
 echo "Installing npm"
 npm install -g npm@10.9.0
+
 echo "Installing hardhat dependencies"
 npm install --save-dev hardhat --no-audit
+
 echo "Installing hardhat plugins"
 npm install --save-dev @nomicfoundation/hardhat-toolbox --no-audit
-#npm install ethers@latest
 
 # Start the hardhat node in the background
-npx hardhat node &
+nohup npx hardhat node > /dev/null 2>&1 &
+nohup bash -c 'while true; do sleep 1; done' &
 
 # Wait for the node to be ready
 sleep 10
 
+# Compile and deploy the contract
+npx hardhat compile
+npx hardhat run scripts/deploy.js --network localhost
+
+sleep 5
+
+ps aux | grep '[n]ode' | awk '{print $2}' | while read pid; do
+    # Kill the process by its PID
+    kill -9 $pid
+done
+
+nohup npx hardhat node &
+# Wait for the node to be ready
+sleep 10
 # Compile and deploy the contract
 npx hardhat compile
 npx hardhat run scripts/deploy.js --network localhost
